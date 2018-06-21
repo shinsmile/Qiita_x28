7df2a9d7f5a2d97c7fa79dac41d54cf02c7b7617
ShinsukeArai <shinsuke.r.arai@gmail.com>
2018-06-21 19:48:18 +0900

    creating-post-article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,31 @@ class ArticlesController < ApplicationController
 
   def index
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    if Article.create(article_params)
+      redirect_to :root
+    else
+      redirect_to action: :new
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(
+      :user_id,
+      :title,
+      :body
+    )
+  end
+
+
 end
--- db/migrate/20180619093133_create_tags.rb
+++ db/migrate/20180619093133_create_tags.rb
@@ -1,4 +1,4 @@
-class CreateTags < ActiveRecord::Migration[4.2][5.2]
+class CreateTags < ActiveRecord::Migration[4.2][4.2][5.2]
   def change
     create_table :tags do |t|
       t.string :tag_name, null: false
