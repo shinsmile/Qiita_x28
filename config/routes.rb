b33353b404cdaddfd10955189e16f8a0e83017a5
ShinsukeArai <shinsuke.r.arai@gmail.com>
2018-06-29 13:08:22 +0900

    updating-settings

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   devise_for :users
   root 'articles#index'
 
-  resources :articles do
-    resources :likes, only:[:create, :destroy]
-  end
-  resources :users
-  
   get '/articles/stocks', to: 'articles#stocks'
   resources :articles do
     resources :stocks
+    resources :likes, only:[:create, :destroy]
   end
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
