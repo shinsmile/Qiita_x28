f99fe75eba58a632af14cc1b506169a9a4903ae8
tepepe61 <akimiatepp61@gmail.com>
2018-07-06 17:42:15 +0900

    UserController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,33 @@
 class UsersController < ApplicationController
+  layout 'main'
+
+  def index
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+
+
+
+# 以下like private commentpageはviewを呼ぶアクション
+# 記述が効率悪い？
+  def like
+   @user = User.find(params[:id])
+  end
+
+  def private
+   @user = User.find(params[:id])
+  end
+
+  def commentpage
+   @user = User.find(params[:id])
+  end
 
 end
