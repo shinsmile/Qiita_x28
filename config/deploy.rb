6bff4afb9fac42905f86ed64fc8dc744d0cb1598
ShinsukeArai <shinsuke.r.arai@gmail.com>
2018-07-14 15:21:52 +0900

    editing production.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.3.1'
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/Users/programming/.ssh/Qiita_28.pem']
+                  keys: ['/Users/programming/.ssh/Qiita_z28.pem']
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
