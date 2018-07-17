78b886ec73fd62bacf1b03ce10301b63bb87a3c6
tatsuya8782 <oimoq13@yahoo.co.jp>
2018-07-17 21:40:45 +0900

    trend完成

--- app/controllers/trend_controller.rb
+++ app/controllers/trend_controller.rb
@@ -4,19 +4,7 @@ layout "main"
 #
 
 def show
-    @article = Article.all
-    #REDIS.zincrby "articles/daily/#{Date.today.to_s}", 1, "#{@article.id}"
+  @article = Article.all.order("likes_count DESC").limit(10)
 end
 
-  #def set_ranking_data
-    #５件のランキングデータを取得
-    #ids = REDIS.zrevrangebyscore "articles/daily/#{Date.today.to_s}", "+inf", 0, limit: [0, 5]
-    #@ranking_articles = Article.where(id: ids)
-
-     #５件未満の場合、公開日時順で値を取得
-    #if @ranking_articles.count < 5
-      #adding_articles = Article.order(publish_time: :DESC, updated_at: :DESC).where.not(id: ids).limit(5 - @ranking_articles.count)
-      #@ranking_articles.concat(adding_articles)
-    #end
-  #end
 end
