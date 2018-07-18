40bc2445ccf5a6fae304e6afbf141f2242771dd8
YO3134 <nishiura.145@gmail.com>
2018-07-18 14:56:09 +0900

    add_function

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_10_061653) do
+ActiveRecord::Schema.define(version: 2018_07_13_032724) do
 
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2018_07_10_061653) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["article_id"], name: "index_likes_on_article_id"
+    t.index ["user_id", "article_id"], name: "index_likes_on_user_id_and_article_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
